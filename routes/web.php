b88d3555c2a7ea3f32adfd1c3193429ad614591b
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-06-03 14:02:22 -0300

    Enviando parametros

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,15 @@ Route::get(
     '/contato',
     [ContatoController::class, 'contato']
 );
+
+Route::get(
+    '/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+    function (
+        string $nome,
+        string $categoria,
+        string $assunto,
+        string $mensagem
+    ) {
+        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+    }
+);
