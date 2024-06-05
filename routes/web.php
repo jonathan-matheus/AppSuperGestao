8ea5d3db5e4a482579152f73c2c9e2d8f36bac29
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-06-05 09:59:11 -0300

    Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,32 @@ use App\Http\Controllers\SobreNosController;
 Route::get(
     '/',
     [PrincipalController::class, 'principal']
-);
+)->name('site.index');
 
 Route::get(
     '/sobre-nos',
     [SobreNosController::class, 'sobreNos']
-);
+)->name('site.sobrenos');
 
 Route::get(
     '/contato',
     [ContatoController::class, 'contato']
-);
+)->name('site.contato');
 
 Route::get('/login', function () {
     return 'Login';
-});
+})->name('site.login');
 
 Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
         return 'Clientes';
-    });
+    })->name('app.clientes');
 
     Route::get('/fornecedores', function () {
         return 'Fornecedores';
-    });
+    })->name('app.fornecedores');
 
     Route::get('/produtos', function () {
         return 'Produtos';
-    });
+    })->name('app.produtos');
 });
