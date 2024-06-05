78e32312e636c8382bba429aa98a5d9c90f4a98b
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-06-05 09:39:08 -0300

    Agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,21 @@ Route::get(
     '/contato',
     [ContatoController::class, 'contato']
 );
+
+Route::get('/login', function () {
+    return 'Login';
+});
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    });
+
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    });
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    });
+});
