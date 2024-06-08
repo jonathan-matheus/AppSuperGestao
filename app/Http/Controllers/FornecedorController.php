915ee3ffff9ed0c6d755b12690072e9f250b14e2
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-06-08 15:50:46 -0300

    isset

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,7 +9,15 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N'],
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '11.111.111/1111-11',
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S'
+            ],
         ];
         return view('app.fornecedores.index', compact('fornecedores'));
     }
